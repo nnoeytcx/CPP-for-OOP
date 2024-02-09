f130e008ab962c695c10dd88b28cd6bb12d94865
nnoeytcx <thitichaya5422@gmail.com>
2024-02-09 20:46:20 +0700

    fix ctrl d

--- c00/ex01/Phonebook.cpp
+++ c00/ex01/Phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 22:30:09 by tpoungla          #+#    #+#             */
-/*   Updated: 2024/01/20 05:13:03 by tpoungla         ###   ########.fr       */
+/*   Updated: 2024/02/09 20:45:17 by tpoungla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@ void Phonebook::add(void)
 
 	for (int i = 0; i < 5; i++)
 	{
+        
 		std::cout << prompt[i] ;
 		std::getline(std::cin, input[i]);
         if (checkInput(input[i]))
         {
+            if (std::cin.eof())
+                break;
             std::cout<<"!Empty Input!"<<std::endl;
             i--;
         }
--- c00/ex01/awesome.cpp
+++ c00/ex01/awesome.cpp
@@ -6,7 +6,7 @@
 /*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:11:21 by tpoungla          #+#    #+#             */
-/*   Updated: 2024/01/20 05:34:43 by tpoungla         ###   ########.fr       */
+/*   Updated: 2024/02/09 20:32:59 by tpoungla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(void)
 	std::string	str;
 	Phonebook	phonebook;
 
-	while (1)
+	while (1 && !std::cin.eof())
 	{
 		std::cout<<"let's use this awesome phonebook : ";
 		std::getline(std::cin, str);
